0400f01a103c49a5216298cab0ce22ab9c929d9a
zhongjiaqi <1434248020@qq.com>
2022-09-18 13:37:06 +0800

    修改密码

--- app/database.php
+++ app/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '49.232.134.206',
     // 数据库名
     'database'        => 'xenon_wms',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => '!@#qwe123',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
